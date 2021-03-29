037755a083238548e0d884fca4f7a77a608989a4
Ehsan-Aghapour <aghapour.ehsan17@gmail.com>
2021-03-29 14:14:13 +0200

    modify test.sh for saving model add data dir

--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
 
+echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
+
+echo 4 > /sys/class/mpgpu/max_freq
+echo 4 > /sys/class/mpgpu/min_freq
+echo "Current GPU min freq:"
+cat /sys/class/mpgpu/min_freq
+echo "Current GPU ferq:"
+cat /sys/class/mpgpu/cur_freq
+
+
+sleep 2
+
+
 export LD_LIBRARY_PATH=/data/data/com.termux/files/home/ARMCL-Khadas/lib/
 
 chmod +x $1
@@ -7,7 +21,10 @@ p="/data/data/com.termux/files/home/ARMCL-Khadas"
 #p=".."
 
 _dt="$p/compute_library_alexnet/"
-_img="$p/one_image/"
+#_dt="/data/data/com.termux/files/home/ARMCL-Khadas/assets/Save/Alexnet_Save/"
+#_img="$p/one_image/"
+#_img="$p/assets/assets_alexnet/ppm_images/"
+_img="/data/data/com.termux/files/home/ARMCL-Khadas/assets/assets_resnet50/ppm_images/"
 _lbl="$p/compute_library_alexnet/labels.txt"
 
 
@@ -15,18 +32,26 @@ lbl=""
 img=""
 dt=""
 
-if [ "$3" ]; then
+if [ "$3" -eq 1 ]; then
     lbl="--labels=$_lbl";
 fi
 
-if [ "$4" ]; then
-    img="--image=$_img"
+if [ "$4" -eq 1 ]; then
+    #img="--image=$_img"
     dt="--data=$_dt"
 fi
+
+
+if [ "$5" -eq 1 ]; then
+    img="--image=$_img"
+fi
+
+
+
 
 echo $img
 echo $dt
 echo $lbl
 
 
-$1 --target=$2 $dt $img $lbl 
+$1 --threads=4  --target=$2 $dt $img $lbl --partition_point=$6 
