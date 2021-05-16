0bc76a75ef19fda04cb6ec2eeec6310884f8caf7
Ehsan-aghapour <48172435+Ehsan-aghapour@users.noreply.github.com>
2021-05-16 18:53:19 +0200

    backup_sync pipline working

--- test.sh
+++ test.sh
@@ -1,4 +1,18 @@
 
+echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
+
+echo 4 > /sys/class/mpgpu/max_freq
+echo 4 > /sys/class/mpgpu/min_freq
+##echo "Current GPU min freq:"
+##cat /sys/class/mpgpu/min_freq
+##echo "Current GPU ferq:"
+##cat /sys/class/mpgpu/cur_freq
+
+
+sleep 2
+
+
 export LD_LIBRARY_PATH=/data/data/com.termux/files/home/ARMCL-Khadas/lib/
 
 chmod +x $1
@@ -6,27 +20,74 @@ chmod +x $1
 p="/data/data/com.termux/files/home/ARMCL-Khadas"
 #p=".."
 
-_dt="$p/compute_library_alexnet/"
-_img="$p/one_image/"
-_lbl="$p/compute_library_alexnet/labels.txt"
+#_dt="$p/compute_library_alexnet/"
+_dt1="$p/assets/Save/alexnet/"
+_dt2="$p/assets/Save/googlenet/"
+_dt3="$p/assets/Save/mobilenet/"
+_dt4="$p/assets/Save/resnet50/"
+_dt5="$p/assets/Save/squeezenet/"
+
+#_img="$p/one_image/"
+#_img="$p/assets/assets_alexnet/ppm_images/"
+#_img="/data/data/com.termux/files/home/ARMCL-Khadas/assets/assets_resnet50/ppm_images/"
+
+_img1="$p/assets/Save/ppm_images_227/"
+_img2="$p/assets/Save/ppm_images_224/"
+
+
+_lbl="$p/assets/Save/labels.txt"
 
 
 lbl=""
 img=""
 dt=""
 
-if [ "$3" ]; then
+if [ "$5" -eq 1 ]; then
     lbl="--labels=$_lbl";
 fi
 
-if [ "$4" ]; then
-    img="--image=$_img"
-    dt="--data=$_dt"
+if [ "$4" -eq 1 ]; then
+    #img="--image=$_img"
+    dt="--data=$_dt1"
 fi
 
-echo $img
-echo $dt
-echo $lbl
+
+if [ "$4" -eq 2 ]; then                                                                
+    #img="--image=$_img"                    
+    dt="--data=$_dt2"     
+fi 
+
+if [ "$4" -eq 3 ]; then                                                                
+    #img="--image=$_img"                    
+    dt="--data=$_dt3"     
+fi 
+
+if [ "$4" -eq 4 ]; then                                                                
+    #img="--image=$_img"                    
+    dt="--data=$_dt4"     
+fi 
+
+if [ "$4" -eq 5 ]; then                                                                
+    #img="--image=$_img"                    
+    dt="--data=$_dt5"     
+fi 
+
+if [ "$3" -eq 1 ]; then
+    img="--image=$_img1"
+fi
+
+if [ "$3" -gt 1 ]; then
+    img="--image=$_img2"
+fi
+
+n=""
+if [ "$6" ]; then
+    n="--n=$6"
+fi
+
+#echo $img
+#echo $dt
+#echo $lbl
 
 
-$1 --target=$2 $dt $img $lbl 
+$1 --threads=4  --target=$2 $dt $img $lbl $n --save=$7 --annotate=$8 --partition_point=$9 
