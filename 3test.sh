06514906b0ebd66ce99578942a63ff0d9775abcd
Ehsan-aghapour <aghapour.ehsan17@gmail.com>
2022-11-14 14:17:46 +0100

    3test.sh

--- 3test.sh
+++ 3test.sh
@@ -3,14 +3,18 @@
 
 
 ######## for Khadas AOSP(CPU) ##############
-#echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-#echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
+##echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+##echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
+#big:
+##echo 2208000 > /sys/devices/system/cpu/cpufreq/policy2/scaling_max_freq
+#little:
+##echo 1800000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 
 ######## for Google AOSP(CPU) ##############
-echo userspace > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo userspace > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
-echo 1800000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
-echo 2208000 > /sys/devices/system/cpu/cpufreq/policy2/scaling_setspeed
+#echo userspace > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+#echo userspace > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor
+#echo 1800000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
+#echo 2208000 > /sys/devices/system/cpu/cpufreq/policy2/scaling_setspeed
 
 
 ######## for Khadas AOSP(GPU) ##############
@@ -18,10 +22,10 @@ echo 2208000 > /sys/devices/system/cpu/cpufreq/policy2/scaling_setspeed
 #echo 4 > /sys/class/mpgpu/min_freq
 
 ######## for Google AOSP(GPU) #########
-echo userspace > /sys/class/devfreq/ffe40000.gpu/governor
-cat /sys/class/devfreq/ffe40000.gpu/governor
-echo 799999987 > /sys/class/devfreq/ffe40000.gpu/userspace/set_freq
-cat /sys/class/devfreq/ffe40000.gpu/cur_freq
+#echo userspace > /sys/class/devfreq/ffe40000.gpu/governor
+#cat /sys/class/devfreq/ffe40000.gpu/governor
+#echo 799999987 > /sys/class/devfreq/ffe40000.gpu/userspace/set_freq
+#cat /sys/class/devfreq/ffe40000.gpu/cur_freq
 
 ##echo "Current GPU min freq:"
 ##cat /sys/class/mpgpu/min_freq
@@ -32,7 +36,7 @@ cat /sys/class/devfreq/ffe40000.gpu/cur_freq
 sleep 2
 
 
-export LD_LIBRARY_PATH=/data/data/com.termux/files/home/ARMCL-Khadas/lib64/
+export LD_LIBRARY_PATH=/data/data/com.termux/files/home/ARMCL-Khadas/lib/
 
 chmod +x $1
 
@@ -40,21 +44,25 @@ p="/data/data/com.termux/files/home/ARMCL-Khadas"
 #p=".."
 
 #_dt="$p/compute_library_alexnet/"
-_dt1="$p/assets/Save/alexnet/"
-_dt2="$p/assets/Save/googlenet/"
-_dt3="$p/assets/Save/mobilenet/"
-_dt4="$p/assets/Save/resnet50/"
-_dt5="$p/assets/Save/squeezenet/"
+S="."
+if [ "$7" -eq 1 ]; then
+	S="Save"
+fi
+_dt1="$p/assets/${S}/alexnet/"
+_dt2="$p/assets/${S}/googlenet/"
+_dt3="$p/assets/${S}/mobilenet/"
+_dt4="$p/assets/${S}/resnet50/"
+_dt5="$p/assets/${S}/squeezenet/"
 
 #_img="$p/one_image/"
 #_img="$p/assets/assets_alexnet/ppm_images/"
 #_img="/data/data/com.termux/files/home/ARMCL-Khadas/assets/assets_resnet50/ppm_images/"
 
-_img1="$p/assets/Save/ppm_images_227/"
-_img2="$p/assets/Save/ppm_images_224/"
+_img1="$p/assets/${S}/ppm_images_227/"
+_img2="$p/assets/${S}/ppm_images_224/"
 
 
-_lbl="$p/assets/Save/labels.txt"
+_lbl="$p/assets/${S}/labels.txt"
 
 
 lbl=""
@@ -110,6 +118,10 @@ fi
 
 #gdbserver :539 
 echo "$1 --threads=4  --threads2=2  --target=$2 $dt $img $lbl $n --save=$7 --annotate=$8 --partition_point=$9 --partition_point2=${10} --order=${11} --layer_time=${12} ${13} ${14} ${15} ${16} ${17}"
+
+#./graph_alexnet_n_pipe --target=NEON --image=/data/data/com.termux/files/home/ARMCL-Khadas/Large/Alex_extracted/images/ --data=/data/data/com.termux/files/home/ARMCL-Khadas/Large/Alex_extracted/ --labels=/data/data/com.termux/files/home/ARMCL-Khadas/Large/Alex_extracted/labels.txt --n=4 --partition_point=8 --partition_point2=8 --order=B-L-G
+
+
 
 
 $1 --threads=4  --threads2=2  --target=$2 $dt $img $lbl $n --save=$7 --annotate=$8 --partition_point=$9 --partition_point2=${10} --order=${11} --layer_time=${12} ${13} ${14} ${15} ${16} ${17} 
